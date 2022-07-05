747c533c9fae372c61b50e111c8b70e350a87efd
Dina Shchobova <dina.shchobova@gmail.com>
2022-07-05 12:04:13 +0300

    feat: add token in context

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,29 @@
-import { ApolloServer } from "apollo-server";
-import { typeDefs } from "./graphql";
-import { resolvers } from "./app.module";
+import {ApolloServer} from "apollo-server";
+import {typeDefs} from "./graphql";
+import {resolvers} from "./app.module";
 import "dotenv/config";
-import { UsersAPI } from "./modules/users/services/users.service";
-import { GenresService } from "./modules/genres/services/genres.service";
+import {UsersService} from "./modules/users/services/users.service";
+import {GenresService} from "./modules/genres/services/genres.service";
+import {ArtistsService} from "./modules/artists/services/artists.service";
 
 const PORT = process.env.PORT || 3000;
 
-const server = new ApolloServer( {
+const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: () => {
     return {
-      userAPI: new UsersAPI(),
-      genresService: new GenresService()
+      usersService: new UsersService(),
+      genresService: new GenresService(),
+      artistsService: new ArtistsService(),
     }
-  }
+  },
+  context: async ({ req }) => {
+    const token = req.headers.authorization || '';
+    console.log(token);
+    return token;
+  },
 });
 
-server.listen({ port: PORT})
-.then(() => console.log(`Server is running on port ${PORT}`))
+server.listen({port: PORT})
+  .then(() => console.log(`Server is running on port ${PORT}`))
