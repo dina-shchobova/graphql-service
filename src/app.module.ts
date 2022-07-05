c07f7bf43ed001741048d35a7e23525126c5089f
Dina Shchobova <dina.shchobova@gmail.com>
2022-07-05 12:08:18 +0300

    fix: fix resolvers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
+import { usersResolver } from "./modules/users/resolvers/users.resolver";
 import { genresResolver } from './modules/genres/resolvers/genres.resolver';
+import { artistsResolver } from './modules/artists/resolvers/artists.resolver';
 
 const resolvers = [
+  usersResolver,
   genresResolver,
+  artistsResolver
 ]
 
 export { resolvers }
