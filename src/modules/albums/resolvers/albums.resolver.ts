85be7ecc5fd5f5c4e0300752875cbb88e921e057
Dina Shchobova <dina.shchobova@gmail.com>
2022-07-09 15:18:15 +0300

    feat: add Album mutation

--- src/modules/albums/resolvers/albums.resolver.ts
+++ src/modules/albums/resolvers/albums.resolver.ts
@@ -1,6 +1,6 @@
 const albumsResolver = {
   Query: {
-    albums: (_, { pagination }, { dataSources }) => {
+    albums: (_, {pagination}, {dataSources}) => {
       if (pagination && pagination.limit && (pagination.offset || pagination.offset === 0)) {
         return dataSources.albumsService.getAlbums(pagination.limit, pagination.offset);
       } else {
@@ -8,35 +8,35 @@ const albumsResolver = {
       }
 
     },
-    album: ( _, {id}, {dataSources}) => {
+    album: (_, {id}, {dataSources}) => {
       return dataSources.albumsService.getAlbum(id);
     }
   },
 
   Album: {
     id: (parent) => parent._id,
-    artists: ({ artistsIds }, _, { dataSources }) => {
+    artists: ({artistsIds}, _, {dataSources}) => {
       const artists = [];
       artistsIds.forEach((id) => {
         artists.push(dataSources.artistsService.getArtist(id));
       })
       return artists;
     },
-    bands: ({ bandsIds }, _, { dataSources }) => {
+    bands: ({bandsIds}, _, {dataSources}) => {
       const bands = [];
       bandsIds.forEach((id) => {
         bands.push(dataSources.bandsService.getBand(id));
       })
       return bands;
     },
-    genres: ({ genresIds }, _, { dataSources }) => {
+    genres: ({genresIds}, _, {dataSources}) => {
       const genres = [];
       genresIds.forEach((id) => {
         genres.push(dataSources.genresService.getGenre(id));
       })
       return genres;
     },
-    tracks: ({ trackIds }, _, { dataSources }) => {
+    tracks: ({trackIds}, _, {dataSources}) => {
       const tracks = [];
       trackIds.forEach((id) => {
         tracks.push(dataSources.tracksService.getTrack(id));
@@ -44,6 +44,33 @@ const albumsResolver = {
       return tracks;
     }
   },
+
+  Mutation: {
+    createAlbum: (_, {albumInput}, {dataSources}) => {
+      try {
+        return dataSources.albumsService.createAlbum(albumInput);
+      } catch (e) {
+        return null;
+      }
+    },
+
+    deleteAlbum: (_, {id}, {dataSources}) => {
+      try {
+        return dataSources.albumsService.deleteAlbum(id);
+      } catch (e) {
+        return null;
+      }
+    },
+
+    updateAlbum: (_, {id, updateAlbumInput}, {dataSources}) => {
+      try {
+        return dataSources.albumsService.updateAlbum(id, updateAlbumInput);
+      } catch (e) {
+        return null;
+      }
+    },
+  }
+
 }
 
-export { albumsResolver };
+export {albumsResolver};
--- src/modules/albums/services/albums.service.ts
+++ src/modules/albums/services/albums.service.ts
@@ -1,10 +1,14 @@
-import { RESTDataSource } from "apollo-datasource-rest";
+import {RESTDataSource} from "apollo-datasource-rest";
 import "dotenv/config";
 
 class AlbumsService extends RESTDataSource {
   constructor() {
     super();
     this.baseURL = process.env.ALBUMS_URL;
+  }
+
+  willSendRequest(request) {
+    request.headers.set('Authorization', `Bearer ${this.context.token}`);
   }
 
   async getAlbums(limit: number = 5, offset: number = 0) {
@@ -23,6 +27,30 @@ class AlbumsService extends RESTDataSource {
       console.log(e);
     }
   }
+
+  async createAlbum(updateAlbumInput) {
+    try {
+      return await this.post(``, updateAlbumInput);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async deleteAlbum(id: string) {
+    try {
+      return await this.delete(`/${id}`);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async updateAlbum(id, albumInput) {
+    try {
+      return await this.put(`/${id}`, albumInput);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
 
-export { AlbumsService };
+export {AlbumsService};
